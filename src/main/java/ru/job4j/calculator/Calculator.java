7b86a1b17178be74e9cef0a3c2e28cbd1d43c00d
SpS <shchaaas@mail.ru>
2022-04-09 19:10:31 +0300

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,20 +1,29 @@
 package ru.job4j.calculator;
 
+import static ru.job4j.math.MathFunction.*;
+
 public class Calculator {
 
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double extructAndDivision(double first, double second) {
+        return extruct(first, second)
+                + devision(first, second);
+    }
+
+    public static double sumMultiplyExtructAndDivision(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second)
+                + extruct(first, second)
+                + devision(first, second);
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = (six / two);
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + extructAndDivision(10, 20));
+        System.out.println("Результат расчета равен: " + sumMultiplyExtructAndDivision(10, 20));
     }
 }
---
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -0,0 +1,20 @@
+package ru.job4j.math;
+
+@SuppressWarnings("checkstyle:EmptyLineSeparator")
+public class MathFunction {
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+
+    public static double extruct(double first, double second) {
+        return first - second;
+    }
+
+    public static double devision(double first, double second) {
+        return first / second;
+    }
+}
