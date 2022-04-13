9fe46d5d38014eed168b120bcce16e5472d9b99f
SpS <shchaaas@mail.ru>
2022-04-13 20:17:46 +0300

    4.6.2. Switch - matcher [#334071]

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -10,6 +10,7 @@ public class MultipleSwitchWeek {
             case "Friday", "Пятница" -> 5;
             case "Saturday", "Суббота" -> 6;
             case "Sunday", "Воскресенье" -> 7;
+            default -> -1;
         };
-   }
+    }
 }
