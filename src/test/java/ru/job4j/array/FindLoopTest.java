aee4061966a72b22dd5573720f5514dfc10b6cb0
SpS <shchaaas@mail.ru>
2022-04-16 15:25:16 +0300

    6.1. Классический поиск перебором. [#33489]

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = {5, 4, 3, 2};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHasNot10ThenMinus1() {
+        int[] data = {5, 4, 3, 2};
+        int el = 10;
+        int actual = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenVoidArrayHasNot10ThenMinus1() {
+        int[] data = {};
+        int el = 10;
+        int actual = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenArrayHasNotMinus10ThenMinus1() {
+        int[] data = {5, 4, 3, 2};
+        int el = -10;
+        int actual = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenVoidArrayHasNotMinus10ThenMinus1() {
+        int[] data = {};
+        int el = -10;
+        int actual = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, actual);
+    }
+}
