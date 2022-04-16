98f3088ff13f6647c7ca9972825e298e5960b5d4
SpS <shchaaas@mail.ru>
2022-04-16 21:37:57 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,14 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int index2 = -1;
+        for (int i = start; i <= finish; i++) {
+            if (el == data[i]) {
+                index2 = i;
+            }
+        }
+        return index2;
+    }
 }
