8f86ee26f2bbdeb1e7a51d85fcc7ac92c082c707
SpS <shchaaas@mail.ru>
2022-04-23 22:09:36 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -26,4 +26,46 @@ public class MaxTest {
         int actual = 3;
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void maxWhen3And2And1Then3() {
+        int expected = Max.max(3, 2, 1);
+        int actual = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxWhen3And3And3Then3() {
+        int expected = Max.max(3, 3, 3);
+        int actual = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxWhenMinus3And3And3And3Then3() {
+        int expected = Max.max(-3, 3, 3, 3);
+        int actual = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxWhenMinus3AndMinus2And3And3Then3() {
+        int expected = Max.max(-3, -2, 3, 3);
+        int actual = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxWhenMinus3AndMinus2AndMinus1And0Then0() {
+        int expected = Max.max(-3, -2, -1, 0);
+        int actual = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxWhenMinus4AndMinus3AndMinus2AndMinus1ThenMinus1() {
+        int expected = Max.max(-4, -3, -2, -1);
+        int actual = -1;
+        assertEquals(expected, actual);
+    }
 }
