a4e53cb0068fe422ca871760c50d115e02764ea2
SpS <shchaaas@mail.ru>
2022-04-24 18:27:38 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643] OOP

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -3,19 +3,25 @@ package ru.job4j.condition;
 public class Point {
     private int x;
     private int y;
+    private int z;
 
-    public Point(int x, int y) {
+    public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
     public double distance(Point that) {
         return Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2));
     }
 
+    public double distance3D(Point that) {
+        return Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2) + Math.pow((this.z - that.z), 2));
+    }
+
     public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(2, 0);
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(2, 0, 0);
         double result = a.distance(b);
         System.out.println("result (0, 0) to (2, 0) " + result);
     }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,27 +8,36 @@ public class PointTest {
 
     @Test
     public void distanceWhenX10Y10X20Y20Then1() {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 1);
-        double actual = a.distance(b);
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 1, 0);
+        double actual = a.distance3D(b);
         double expected = 1;
         assertEquals(expected, actual, 0.01);
     }
 
     @Test
     public void distanceWhenX1minus1Y1minus3X20Y20Then4Dot12() {
-        Point a = new Point(-1, -3);
-        Point b = new Point(0, 1);
-        double actual = a.distance(b);
-        double expected = 4.12;
+        Point a = new Point(-1, -3, -2);
+        Point b = new Point(0, 1, 2);
+        double actual = a.distance3D(b);
+        double expected = 5.74;
         assertEquals(expected, actual, 0.01);
     }
 
     @Test
     public void distanceWhenX1minus2Y1minus3X2minus4Y2minus5Then2Dot82() {
-        Point a = new Point(-2, -3);
-        Point b = new Point(-4, -5);
-        double actual = a.distance(b);
+        Point a = new Point(-2, -3, -1);
+        Point b = new Point(-4, -5, 7);
+        double actual = a.distance3D(b);
+        double expected = 8.48;
+        assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+    public void distanceWhenX1minus2Y1minus3Z10X2minus4Y2minus5Z20Then2Dot82() {
+        Point a = new Point(-2, -3, 0);
+        Point b = new Point(-4, -5, 0);
+        double actual = a.distance3D(b);
         double expected = 2.82;
         assertEquals(expected, actual, 0.01);
     }
